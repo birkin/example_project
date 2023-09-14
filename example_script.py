dc0b4b70751aa6b6f3e8b840485764bba371cda6
Birkin James Diana <birkin.diana@gmail.com>
2023-09-14 16:25:09 -0400

    stubs structure.

--- example_script.py
+++ example_script.py
@@ -1,5 +1,34 @@
-import os
+import logging, os, sys
 import requests
 
+## setup ------------------------------------
 auth_key = os.environ['ES__AUTH_KEY']
-print(f'the auth-key is, ``{auth_key}``')
+print(f'in x() funtion; the auth-key is, ``{auth_key}``')
+
+## create basicConfig logger with no file-writer
+logging.basicConfig(
+    level=logging.INFO,
+    # format='%(asctime)s %(levelname)s %(message)s',
+    format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
+    datefmt='%d/%b/%Y %H:%M:%S',
+    handlers=[ logging.StreamHandler(sys.stdout) ]
+)
+log = logging.getLogger(__name__)
+log.debug( 'test log entry' )
+
+
+## ok real work ----------------------------
+
+## make api-request
+rsp = requests.get( 'https://api.weather.gov/gridpoints/OKX/33,34/forecast' )
+json_data = rsp.json()
+
+## parse out the fahrenheit temp
+pass
+f_temp = 32
+pass
+
+## convert to celsius
+pass
+c_temp = 0
+pass
