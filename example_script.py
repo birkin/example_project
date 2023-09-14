9b32c5a09932a6fb20476598b82838170335a802
Birkin James Diana <birkin.diana@gmail.com>
2023-09-14 16:48:27 -0400

    adds better structure.

--- example_script.py
+++ example_script.py
@@ -1,13 +1,14 @@
 import logging, os, sys
 import requests
 
+print( 'starting' )
 ## setup ------------------------------------
 auth_key = os.environ['ES__AUTH_KEY']
-print(f'in x() funtion; the auth-key is, ``{auth_key}``')
+# print(f'in x() funtion; the auth-key is, ``{auth_key}``')
 
 ## create basicConfig logger with no file-writer
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     # format='%(asctime)s %(levelname)s %(message)s',
     format='[%(asctime)s] %(levelname)s [%(module)s-%(funcName)s()::%(lineno)d] %(message)s',
     datefmt='%d/%b/%Y %H:%M:%S',
@@ -17,18 +18,41 @@ log = logging.getLogger(__name__)
 log.debug( 'test log entry' )
 
 
-## ok real work ----------------------------
+## ok real work ----------------------------\
 
-## make api-request
-rsp = requests.get( 'https://api.weather.gov/gridpoints/OKX/33,34/forecast' )
-json_data = rsp.json()
+def manage_work():
 
-## parse out the fahrenheit temp
-pass
-f_temp = 32
-pass
+    ## make api-request
+    data = make_api_request()
 
-## convert to celsius
-pass
-c_temp = 0
-pass
+    ## parse out the fahrenheit temp
+    f_temp = parse_out_fahrenheit_temp( data )
+
+    ## convert to celsius
+    c_temp = convert_to_celsius(f_temp)
+    
+    log.debug( f'c_temp, ``{c_temp}``' )
+    return c_temp
+
+
+def make_api_request():
+    return 'foo'
+
+
+def parse_out_fahrenheit_temp( the_data ):
+    x = the_data[0]
+    return 32
+
+def convert_to_celsius( f_temp ):
+    """ 
+    Returns celsius temp as int 
+
+    Usage:
+    >>> convert_to_celsius( 32 )
+    0
+    """
+    return 0
+
+
+if __name__ == '__main__':
+    manage_work()
